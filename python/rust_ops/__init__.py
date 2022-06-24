aa204d57450f9d19d5d8ef09047b8dfcd45bb620
Daniël de Kok <me@danieldk.eu>
2022-06-24 16:14:59 +0200

    Add basic SIMD scaffolding
    This incomplete scaffolding is to explore the solution space. We
    currently support three instruction sets:
    
    - AVX
    - NEON
    - scalar
    
    And the following element-wise activations:
    
    - ReLU
    - Hard sigmoid
    - Hard tanh
    - clipped linear
    
    We need to add more instruction sets (e.g. SSE), but I want to keep
    things small until we have cleaned up the implementation a little.

--- python/rust_ops/__init__.py
+++ python/rust_ops/__init__.py
@@ -1,8 +1,13 @@
+from typing import TypeVar, cast
 import numpy
 from thinc.api import NumpyOps
-from thinc.types import Xp, DeviceTypes
+from thinc.types import Xp, DeviceTypes, FloatsXd
 
 from .rust_ops import RustOps as _RustOps
+
+
+FloatsType = TypeVar("FloatsType", bound=FloatsXd)
+
 
 class RustOps(_RustOps, NumpyOps):
     name = "rust"
@@ -11,3 +16,19 @@ class RustOps(_RustOps, NumpyOps):
     def __init__(self, device_type: DeviceTypes = "cpu", device_id: int = -1):
         self.device_type = device_type
         self.device_id = device_id
+
+
+    # Remove once https://github.com/explosion/thinc/pull/709 is merged and we
+    # have a Thinc version with this change. Patching sigmoid here to make sure
+    # that we can pass tests.
+    def sigmoid(self, X: FloatsType, *, inplace: bool = False) -> FloatsType:
+        if inplace:
+            # To prevent overflows and help with regularization/numerical stability
+            X = self.xp.clip(X, -20.0, 20.0, out=X)
+            self.xp.exp(-X, out=X)
+            X += 1.0  # type: ignore[assignment]
+            X **= -1.0  # type: ignore[assignment]
+            return cast(FloatsType, X)
+        else:
+            X = self.xp.clip(X, -20.0, 20.0)
+            return cast(FloatsType, 1.0 / (1.0 + self.xp.exp(-X)))
---
+++ src/simd/mod.rs
@@ -0,0 +1,4 @@
+mod array;
+pub(crate) mod vector;
+
+pub(crate) use array::{platform_arrays, Array};
