8bd4f51e2762ec38892070231218775d6e47dc6a
Daniël de Kok <me@danieldk.eu>
2022-07-12 07:50:46 +0200

    Move implementations of SimdVector to separate files

---
+++ simd-array/src/vector/avx2.rs
@@ -0,0 +1,321 @@
+use std::arch::x86_64::{
+    __m256, __m256d, __m256i, _mm256_add_pd, _mm256_add_ps, _mm256_and_pd, _mm256_and_ps,
+    _mm256_andnot_pd, _mm256_andnot_ps, _mm256_castsi256_pd, _mm256_castsi256_ps,
+    _mm256_cmp_pd, _mm256_cmp_ps, _mm256_cvtps_epi32, _mm256_div_pd, _mm256_div_ps,
+    _mm256_floor_pd, _mm256_floor_ps, _mm256_fmadd_pd, _mm256_fmadd_ps, _mm256_loadu_pd,
+    _mm256_loadu_ps, _mm256_loadu_si256, _mm256_max_pd, _mm256_max_ps, _mm256_min_pd,
+    _mm256_min_ps, _mm256_mul_pd, _mm256_mul_ps, _mm256_or_pd, _mm256_or_ps, _mm256_set1_epi32,
+    _mm256_set1_epi64x, _mm256_set1_pd, _mm256_set1_ps, _mm256_store_pd, _mm256_store_ps,
+    _mm256_storeu_pd, _mm256_storeu_ps, _mm256_sub_pd, _mm256_sub_ps, _mm256_xor_pd,
+    _mm256_xor_ps, _CMP_EQ_OQ, _CMP_GT_OQ, _CMP_LT_OQ,
+};
+use std::mem;
+use std::ops::Neg;
+
+use aligned::{Aligned, A32};
+use num_traits::{Float, Zero};
+
+use super::SimdVector;
+use super::scalar::{ScalarVector32, ScalarVector64};
+
+#[derive(Default)]
+pub struct AVX2Vector32;
+
+impl SimdVector for AVX2Vector32 {
+    type Lower = ScalarVector32;
+    type Float = __m256;
+    type FloatScalar = f32;
+    type FloatScalarArray = Aligned<
+        A32,
+        [Self::FloatScalar;
+            mem::size_of::<Self::Float>() / mem::size_of::<Self::FloatScalar>()],
+    >;
+    type Int = __m256i;
+    type IntScalar = i32;
+    type Mask = __m256;
+
+    #[target_feature(enable = "avx2")]
+    unsafe fn abs(a: Self::Float) -> Self::Float {
+        let mask = _mm256_set1_epi32(0x7fffffff);
+        _mm256_and_ps(a, _mm256_castsi256_ps(mask))
+    }
+
+    #[target_feature(enable = "avx2")]
+    unsafe fn add(a: Self::Float, b: Self::Float) -> Self::Float {
+        _mm256_add_ps(a, b)
+    }
+
+    #[target_feature(enable = "avx2")]
+    unsafe fn add_scalar(a: Self::Float, b: f32) -> Self::Float {
+        let b_simd = _mm256_set1_ps(b);
+        _mm256_add_ps(a, b_simd)
+    }
+
+    #[target_feature(enable = "avx2")]
+    unsafe fn bitwise_select(a: Self::Mask, b: Self::Float, c: Self::Float) -> Self::Float {
+        // Self::Float::from_bits((a & b.to_bits()) | ((!a) & c.to_bits()))
+        let u = _mm256_and_ps(a, b);
+        let v = _mm256_andnot_ps(a, c);
+        _mm256_or_ps(u, v)
+    }
+
+    #[target_feature(enable = "avx2")]
+    unsafe fn copy_sign(sign_src: Self::Float, dest: Self::Float) -> Self::Float {
+        // Negative zero has all bits unset, except the sign bit.
+        let sign_bit_mask = Self::splat(Self::FloatScalar::zero().neg());
+        Self::bitwise_select(sign_bit_mask, sign_src, dest)
+    }
+
+    #[target_feature(enable = "avx2")]
+    unsafe fn div(a: Self::Float, b: Self::Float) -> Self::Float {
+        _mm256_div_ps(a, b)
+    }
+
+    #[target_feature(enable = "avx2")]
+    unsafe fn floor(a: Self::Float) -> Self::Float {
+        _mm256_floor_ps(a)
+    }
+
+    #[target_feature(enable = "fma")]
+    unsafe fn fma(a: Self::Float, b: Self::Float, c: Self::Float) -> Self::Float {
+        _mm256_fmadd_ps(a, b, c)
+    }
+
+    #[target_feature(enable = "avx2")]
+    unsafe fn eq(a: Self::Float, b: Self::Float) -> Self::Mask {
+        _mm256_cmp_ps::<_CMP_EQ_OQ>(a, b)
+    }
+
+    #[target_feature(enable = "avx2")]
+    unsafe fn gt(a: Self::Float, b: Self::Float) -> Self::Mask {
+        _mm256_cmp_ps::<_CMP_GT_OQ>(a, b)
+    }
+
+    #[target_feature(enable = "avx2")]
+    unsafe fn lt(a: Self::Float, b: Self::Float) -> Self::Mask {
+        _mm256_cmp_ps::<_CMP_LT_OQ>(a, b)
+    }
+
+    #[target_feature(enable = "avx2")]
+    unsafe fn mul(a: Self::Float, b: Self::Float) -> Self::Float {
+        _mm256_mul_ps(a, b)
+    }
+
+    #[target_feature(enable = "avx2")]
+    unsafe fn mul_scalar(a: Self::Float, b: f32) -> Self::Float {
+        let b_simd = _mm256_set1_ps(b);
+        _mm256_mul_ps(a, b_simd)
+    }
+
+    #[target_feature(enable = "avx2")]
+    unsafe fn neg(a: Self::Float) -> Self::Float {
+        let neg_zero = _mm256_set1_ps(Self::FloatScalar::neg_zero());
+        _mm256_xor_ps(a, neg_zero)
+    }
+
+    #[target_feature(enable = "avx2")]
+    unsafe fn sub(a: Self::Float, b: Self::Float) -> Self::Float {
+        _mm256_sub_ps(a, b)
+    }
+
+    #[target_feature(enable = "avx2")]
+    unsafe fn vmax(a: Self::Float, b: Self::Float) -> Self::Float {
+        _mm256_max_ps(a, b)
+    }
+
+    #[target_feature(enable = "avx2")]
+    unsafe fn vmin(a: Self::Float, b: Self::Float) -> Self::Float {
+        _mm256_min_ps(a, b)
+    }
+
+    #[target_feature(enable = "avx2")]
+    unsafe fn splat(v: f32) -> Self::Float {
+        _mm256_set1_ps(v)
+    }
+
+    #[target_feature(enable = "avx2")]
+    unsafe fn reinterpret_float_signed(v: Self::Int) -> Self::Float {
+        _mm256_castsi256_ps(v)
+    }
+
+    #[target_feature(enable = "avx2")]
+    unsafe fn to_int(v: Self::Float) -> Self::Int {
+        _mm256_cvtps_epi32(v)
+    }
+
+    unsafe fn to_float_scalar_array(v: Self::Float) -> Self::FloatScalarArray {
+        let mut a: Aligned<A32, _> = Aligned([0f32; 8]);
+        _mm256_store_ps(a.as_mut_ptr(), v);
+        a
+    }
+
+    #[target_feature(enable = "avx2", enable = "fma")]
+    unsafe fn with_load_store(f: &impl Fn(Self::Float) -> Self::Float, a: &mut [f32]) {
+        let mut val = _mm256_loadu_ps(a.as_ptr());
+        val = f(val);
+        _mm256_storeu_ps(a.as_mut_ptr(), val);
+    }
+
+    #[target_feature(enable = "avx2", enable = "fma")]
+    unsafe fn apply_elementwise(
+        f: impl Fn(Self::Float) -> Self::Float,
+        f_rest: impl Fn(&mut [f32]),
+        a: &mut [f32],
+    ) {
+        let v = Self;
+        super::apply_elementwise_generic(&v, f, f_rest, a);
+    }
+}
+
+#[derive(Default)]
+pub struct AVX2Vector64;
+
+impl SimdVector for AVX2Vector64 {
+    type Lower = ScalarVector64;
+    type Float = __m256d;
+    type FloatScalar = f64;
+    type FloatScalarArray = Aligned<
+        A32,
+        [Self::FloatScalar;
+            mem::size_of::<Self::Float>() / mem::size_of::<Self::FloatScalar>()],
+    >;
+    type Int = __m256i;
+    type IntScalar = i64;
+    type Mask = __m256d;
+
+    #[target_feature(enable = "avx")]
+    unsafe fn abs(a: Self::Float) -> Self::Float {
+        let mask = _mm256_set1_epi64x(0x7fffffffffffffff);
+        _mm256_and_pd(a, _mm256_castsi256_pd(mask))
+    }
+
+    #[target_feature(enable = "avx")]
+    unsafe fn add(a: Self::Float, b: Self::Float) -> Self::Float {
+        _mm256_add_pd(a, b)
+    }
+
+    #[target_feature(enable = "avx")]
+    unsafe fn add_scalar(a: Self::Float, b: f64) -> Self::Float {
+        let b_simd = _mm256_set1_pd(b);
+        _mm256_add_pd(a, b_simd)
+    }
+
+    #[target_feature(enable = "avx")]
+    unsafe fn bitwise_select(a: Self::Mask, b: Self::Float, c: Self::Float) -> Self::Float {
+        let u = _mm256_and_pd(a, b);
+        let v = _mm256_andnot_pd(a, c);
+        _mm256_or_pd(u, v)
+    }
+
+    #[target_feature(enable = "avx")]
+    unsafe fn copy_sign(sign_src: Self::Float, dest: Self::Float) -> Self::Float {
+        // Negative zero has all bits unset, except the sign bit.
+        let sign_bit_mask = Self::splat(Self::FloatScalar::zero().neg());
+        Self::bitwise_select(sign_bit_mask, sign_src, dest)
+    }
+
+    #[target_feature(enable = "avx")]
+    unsafe fn div(a: Self::Float, b: Self::Float) -> Self::Float {
+        _mm256_div_pd(a, b)
+    }
+
+    #[target_feature(enable = "avx")]
+    unsafe fn floor(a: Self::Float) -> Self::Float {
+        _mm256_floor_pd(a)
+    }
+
+    #[target_feature(enable = "avx", enable = "fma")]
+    unsafe fn fma(a: Self::Float, b: Self::Float, c: Self::Float) -> Self::Float {
+        _mm256_fmadd_pd(a, b, c)
+    }
+
+    #[target_feature(enable = "avx")]
+    unsafe fn eq(a: Self::Float, b: Self::Float) -> Self::Mask {
+        _mm256_cmp_pd::<_CMP_EQ_OQ>(a, b)
+    }
+
+    #[target_feature(enable = "avx")]
+    unsafe fn gt(a: Self::Float, b: Self::Float) -> Self::Mask {
+        _mm256_cmp_pd::<_CMP_GT_OQ>(a, b)
+    }
+
+    #[target_feature(enable = "avx")]
+    unsafe fn lt(a: Self::Float, b: Self::Float) -> Self::Mask {
+        _mm256_cmp_pd::<_CMP_LT_OQ>(a, b)
+    }
+
+    #[target_feature(enable = "avx")]
+    unsafe fn mul(a: Self::Float, b: Self::Float) -> Self::Float {
+        _mm256_mul_pd(a, b)
+    }
+
+    #[target_feature(enable = "avx")]
+    unsafe fn mul_scalar(a: Self::Float, b: f64) -> Self::Float {
+        let b_simd = _mm256_set1_pd(b);
+        _mm256_mul_pd(a, b_simd)
+    }
+
+    #[target_feature(enable = "avx")]
+    unsafe fn neg(a: Self::Float) -> Self::Float {
+        let neg_zero = _mm256_set1_pd(Self::FloatScalar::neg_zero());
+        _mm256_xor_pd(a, neg_zero)
+    }
+
+    #[target_feature(enable = "avx")]
+    unsafe fn sub(a: Self::Float, b: Self::Float) -> Self::Float {
+        _mm256_sub_pd(a, b)
+    }
+
+    #[target_feature(enable = "avx")]
+    unsafe fn vmax(a: Self::Float, b: Self::Float) -> Self::Float {
+        _mm256_max_pd(a, b)
+    }
+
+    #[target_feature(enable = "avx")]
+    unsafe fn vmin(a: Self::Float, b: Self::Float) -> Self::Float {
+        _mm256_min_pd(a, b)
+    }
+
+    #[target_feature(enable = "avx")]
+    unsafe fn splat(v: f64) -> Self::Float {
+        _mm256_set1_pd(v)
+    }
+
+    #[target_feature(enable = "avx")]
+    unsafe fn reinterpret_float_signed(v: Self::Int) -> Self::Float {
+        _mm256_castsi256_pd(v)
+    }
+
+    #[target_feature(enable = "avx")]
+    unsafe fn to_int(v: Self::Float) -> Self::Int {
+        // Blegh, no instruction for this before AVX-512.
+        let mut data_f64 = [0f64; 4];
+        _mm256_storeu_pd(data_f64.as_mut_ptr(), v);
+        let data = data_f64.map(|v| v as i64);
+        _mm256_loadu_si256(data.as_ptr().cast())
+    }
+
+    #[target_feature(enable = "avx")]
+    unsafe fn to_float_scalar_array(v: Self::Float) -> Self::FloatScalarArray {
+        let mut a: Aligned<A32, _> = Aligned([0f64; 4]);
+        _mm256_store_pd(a.as_mut_ptr(), v);
+        a
+    }
+
+    #[target_feature(enable = "avx2", enable = "fma")]
+    unsafe fn with_load_store(f: &impl Fn(Self::Float) -> Self::Float, a: &mut [f64]) {
+        let mut val = _mm256_loadu_pd(a.as_ptr());
+        val = f(val);
+        _mm256_storeu_pd(a.as_mut_ptr(), val);
+    }
+
+    #[target_feature(enable = "avx2", enable = "fma")]
+    unsafe fn apply_elementwise(
+        f: impl Fn(Self::Float) -> Self::Float,
+        f_rest: impl Fn(&mut [f64]),
+        a: &mut [f64],
+    ) {
+        let v = Self;
+        super::apply_elementwise_generic(&v, f, f_rest, a);
+    }
+}
---
+++ simd-array/src/vector/mod.rs
@@ -0,0 +1,149 @@
+use as_slice::AsSlice;
+use std::fmt::Debug;
+use std::mem;
+
+use num_traits::{Float, FloatConst, NumCast, PrimInt};
+
+pub trait FloatingPointProps {
+    fn bias() -> usize;
+
+    fn mantissa_bits() -> usize;
+
+    fn max_ln() -> Self;
+}
+
+impl FloatingPointProps for f32 {
+    fn bias() -> usize {
+        127
+    }
+
+    fn mantissa_bits() -> usize {
+        23
+    }
+
+    fn max_ln() -> Self {
+        2f32.powi(f32::MAX_EXP - 1).ln()
+    }
+}
+
+impl FloatingPointProps for f64 {
+    fn bias() -> usize {
+        1023
+    }
+
+    fn mantissa_bits() -> usize {
+        52
+    }
+
+    fn max_ln() -> Self {
+        2f64.powi(f64::MAX_EXP - 1).ln()
+    }
+}
+
+pub trait SimdVector: Default + Send + Sync {
+    type Lower: SimdVector<FloatScalar = Self::FloatScalar>;
+    type Float: Copy;
+    type FloatScalar: Debug + Float + FloatConst + FloatingPointProps;
+    type FloatScalarArray: AsSlice<Element = Self::FloatScalar>;
+    type Int: Copy;
+    type IntScalar: PrimInt;
+    type Mask: Copy;
+
+    /// Absolute value.
+    unsafe fn abs(a: Self::Float) -> Self::Float;
+
+    unsafe fn add(a: Self::Float, b: Self::Float) -> Self::Float;
+
+    /// Add a scalar to every vector element.
+    unsafe fn add_scalar(a: Self::Float, b: Self::FloatScalar) -> Self::Float;
+
+    /// Select bits which are set in `a` in `b` or otherwise `c`.
+    unsafe fn bitwise_select(a: Self::Mask, b: Self::Float, c: Self::Float) -> Self::Float;
+
+    unsafe fn copy_sign(sign_src: Self::Float, dest: Self::Float) -> Self::Float;
+
+    unsafe fn div(a: Self::Float, b: Self::Float) -> Self::Float;
+
+    /// Fused mutiply-add, a * b + c
+    unsafe fn fma(a: Self::Float, b: Self::Float, c: Self::Float) -> Self::Float;
+
+    /// Round to largest integers lower than or equal to the given numbers.
+    unsafe fn floor(a: Self::Float) -> Self::Float;
+
+    /// Convert from f64 into the scalar float type and then splat.
+    unsafe fn from_f64(a: f64) -> Self::Float {
+        Self::splat(<Self::FloatScalar as NumCast>::from(a).unwrap())
+    }
+
+    unsafe fn eq(a: Self::Float, b: Self::Float) -> Self::Mask;
+
+    /// If a is greater than b, set all corresponding lanes to 1.
+    unsafe fn gt(a: Self::Float, b: Self::Float) -> Self::Mask;
+
+    /// If a is less than b, set all corresponding lanes to 1.
+    unsafe fn lt(a: Self::Float, b: Self::Float) -> Self::Mask;
+
+    /// Vector element-wise multiplication.
+    unsafe fn mul(a: Self::Float, b: Self::Float) -> Self::Float;
+
+    /// Multiply every vector element by a scalar.
+    unsafe fn mul_scalar(a: Self::Float, b: Self::FloatScalar) -> Self::Float;
+
+    unsafe fn neg(a: Self::Float) -> Self::Float;
+
+    unsafe fn sub(a: Self::Float, b: Self::Float) -> Self::Float;
+
+    /// Vector element-wise maximum.
+    unsafe fn vmax(a: Self::Float, b: Self::Float) -> Self::Float;
+
+    /// Vector element-wise minimum.
+    unsafe fn vmin(a: Self::Float, b: Self::Float) -> Self::Float;
+
+    unsafe fn splat(v: Self::FloatScalar) -> Self::Float;
+
+    unsafe fn reinterpret_float_signed(v: Self::Int) -> Self::Float;
+
+    unsafe fn to_int(v: Self::Float) -> Self::Int;
+
+    unsafe fn to_float_scalar_array(v: Self::Float) -> Self::FloatScalarArray;
+
+    unsafe fn with_load_store(f: &impl Fn(Self::Float) -> Self::Float, a: &mut [Self::FloatScalar]);
+
+    unsafe fn apply_elementwise(
+        f: impl Fn(Self::Float) -> Self::Float,
+        f_rest: impl Fn(&mut [Self::FloatScalar]),
+        a: &mut [Self::FloatScalar],
+    );
+}
+
+// TODO: get rid of the first argument. Needed so far to help type inference.
+unsafe fn apply_elementwise_generic<V>(
+    _v: &V,
+    f: impl Fn(V::Float) -> V::Float,
+    f_rest: impl Fn(&mut [V::FloatScalar]),
+    mut a: &mut [V::FloatScalar],
+) where
+    V: SimdVector,
+{
+    let elem_size = mem::size_of::<V::Float>() / mem::size_of::<V::FloatScalar>();
+
+    while a.len() >= elem_size {
+        V::with_load_store(&f, a);
+        a = &mut a[elem_size..];
+    }
+
+    if a.len() > 0 {
+        f_rest(a);
+    }
+}
+
+pub mod scalar;
+
+#[cfg(all(target_arch = "x86_64"))]
+pub mod avx;
+
+#[cfg(all(target_arch = "x86_64"))]
+pub mod avx2;
+
+#[cfg(all(target_arch = "aarch64"))]
+pub mod neon;
